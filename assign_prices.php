1a3e705f05eb3b0bc262ac51525b4b4a3bc35510
Kylle Raphael Y. Sunga <kylleraphaelsunga@gmail.com>
2025-11-29 21:53:24 +0800

    Revise shoe prices page branding and title
    Updated the shoe prices page with new branding and title.

--- assign_prices.php
+++ assign_prices.php
@@ -25,7 +25,7 @@ $shoes = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shoe Prices</title>
+    <title>Raphael's KicksLab Prices</title>
     <link rel="stylesheet" href="css/assign_styles.css">
 </head>
 <body>
@@ -67,3 +67,4 @@ $shoes = [
 
 </body>
 </html>
+
