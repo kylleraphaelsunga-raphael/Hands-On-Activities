942b29c7ea9707e0fa05e790f0584f94113f66df
Kylle Raphael Y. Sunga <kylleraphaelsunga@gmail.com>
2025-11-29 16:04:22 +0800

    Add files via upload

---
+++ includes/assign_header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+</head>
+<body>
+
+<header>
+    <h1>Raphael's Shoe Store</h1>
+    <nav>
+        <a href="assign_home.php">Home</a> |
+        <a href="assign_options.php">Shoes</a> |
+        <a href="assign_prices.php">Prices</a> |
+        <a href="assign_stocks.php">Stocks</a>
+    </nav>
+</header>
